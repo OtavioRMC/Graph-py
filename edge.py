cd944247b73b4fa8ad1fdbb1a3d28acb27c9c4b3
Otávio Ribeiro <otavioribeiromoreiradacosta@gmail.com>
2023-12-11 20:34:49 -0300

    feat: Edges now are weight

--- edge.py
+++ edge.py
@@ -10,12 +10,15 @@ class Edge:
   """
   start_vertex: int # o vértice "de"
   end_vertex: int # o vértice "para"
-
+  weight: float
+  
   def __post_init__(self):
-    if not isinstance(self.u, int) or not isinstance(self.v, int):
+    if not isinstance(self.start_vertex, int) or not isinstance(self.end_vertex, int):
       raise TypeError("Os Vértices devem ser inteiros.")
-    if self.u < 0 or self.v < 0:
+    if self.start_vertex < 0 or self.end_vertex < 0:
       raise ValueError("Os Vértices devem ser números inteiros positivos.")
+    if not isinstance(self.weight, (int,float)):
+      raise TypeError("Os pesos devem ser um número.")
 
   def reversed(self) -> Edge:
     """
@@ -24,11 +27,11 @@ class Edge:
     Ela pega a instância atual (referida por self) e retorna uma nova
     instância da Edge onde os vértices u e v são trocados.
     """
-    return Edge(self.v, self.u)
+    return Edge(self.end_vertex, self.start_vertex,self.weight)
   
   def __str__(self) -> str:
     """
     O método __str__ em Python é um método especial que é usado para fornecer uma representação de string
     de um objeto.
     """
-    return f"{self.u} -> {self.v}"
+    return f"{self.start_vertex} --[{self.weight}]--> {self.end_vertex}"
