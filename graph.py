ad567261956577164a75807b1061b8794f648c6f
Otávio Ribeiro <otavioribeiromoreiradacosta@gmail.com>
2023-12-11 08:06:13 -0300

    feat: Added methods for adding adges and vertices.

--- graph.py
+++ graph.py
@@ -37,3 +37,44 @@ class Graph(Generic[V]):
     Aplica a função len para cada um dos vértices.
     """
     return sum(map(len,self._edges))
+  
+  def add_vertex(self, vertex: V) -> int:
+    """
+      Adiciona um vértice ao gráfo e retorna o seu índice.
+      self._edges.append([]) adiciona uma lista vazia para conter as arestas.
+      returna: O índice do vértice adicionado.
+    """
+    self._vertices.append(vertex)
+    self._edges.append([])
+    return self.vertex_count - 1 
+  
+  def add_edge(self, edge: Edge) -> None:
+    """
+    Este é um grafo não direcionado portanto sempre
+    adicionamos arestas nas duas direções.
+    """
+    self._edges[edge.start_vertex].append(edge)
+    self._edges[edge.end_vertex].append(edge.reversed())
+
+  def add_edge_by_indices(self, start_vertex:int, end_vertex:int) -> None:
+    """
+    Adiciona uma aresta usando índices dos vértices(método auxiliar)
+    """
+    edge: Edge = Edge(start_vertex,end_vertex)
+    self.add_edge(edge)
+
+  def add_edge_by_vertices(self, first: V, second: V) -> None:
+    """
+    Adiciona uma aresta consultando os índices dos vértices (método auxiliar)
+    """
+    start_vertex: int = self._vertices.index(first)
+    end_vertex: int = self._vertices.index(second)
+    self.add_edge_by_indices(first, second)
+
+  def vertex_at(self, index:int) -> V:
+    """
+    Encontra o vértice em um índice específico.
+    """
+    return self._vertices[index]
+  
+  
